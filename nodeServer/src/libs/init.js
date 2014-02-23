62138468e2d9678acb3dc0674ad36f8bdb39e30f
Trevor <trevorjbaron@gmail.com>
2014-02-23 01:32:53 -0500

    parse image url to get background

--- nodeServer/src/libs/init.js
+++ nodeServer/src/libs/init.js
@@ -1,8 +1,16 @@
 $(document).ready(onReady)
+
+function getURLParameter(name) {
+	var ret = decodeURIComponent((RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]);
+  return ret == "null" ? null : ret;
+}
+
 function onReady()
 {
 	global.screen = new Screen(800, 600);
-
+	if(getURLParameter("imageUrl")){
+		new Background(0, 0, config.width, config.height, getURLParameter("imageUrl"));
+	}
 	global.char_player = new Character(config.width/2-250, 100, 500, 500, "assets/characters/player");
 	//global.char_computer = new Character(config.width/2-250, 100, 500, 500, "assets/characters/computer");
 
